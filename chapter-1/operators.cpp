28f44e190600e936cbbbd31745de4a506fa95b6e
zakwan hisham <hishamzakwan@gmail.com>
2023-03-03 15:30:02 +0800

    Operators

---
+++ chapter-1/operators.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+/* To learn more about operator and all the symbol, go to:
+ * https://cplusplus.com/doc/tutorial/operators/
+ */
+int main() {
+  int a, b, c;
+  a = 2;
+  b = 7;
+  c = (a > b) ? a : b;
+
+  cout << c << endl;
+}

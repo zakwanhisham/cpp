85c755d2dabee639d36d5528bb43a15b8f2ee0fc
zakwan hisham <hishamzakwan@gmail.com>
2023-03-03 15:15:00 +0800

    Variable

--- chapter-1/variables.cpp
+++ chapter-1/variables.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+// To use string, need to include `string`
 #include <string>
 using namespace std;
 
 int main() {
   // declaring variable
+  /* Different way to declare a variable
+   * int a = 5;
+   * int b(3);
+   * int c{2};
+   * int result;
+   */
   int a, b;
   int result;
 

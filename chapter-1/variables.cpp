12c4ce8566d1221f59b549127038308fcba513b1
zakwan hisham <hishamzakwan@gmail.com>
2023-03-03 15:02:59 +0800

    Variable

---
+++ chapter-1/variables.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  // declaring variable
+  int a, b;
+  int result;
+
+  // process
+  a = 5;
+  b = 2;
+  a += 1;
+  result = a - b;
+
+  // print out the result
+  cout << result << endl;
+
+  // declare string
+  string mystring;
+  mystring = "This is a string";
+  cout << mystring << endl;
+  mystring = "This is a different string";
+  cout << mystring << endl;
+  // terminate program
+  return 0;
+}
